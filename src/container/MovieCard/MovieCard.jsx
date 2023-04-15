205d49ffc3384b77e8ba7e8ed1687b95991ccdc0
Michael Chilaka <obichilaka@gmail.com>
2023-04-15 06:00:28 +0100

    updates on components

--- src/container/MovieCard/MovieCard.jsx
+++ src/container/MovieCard/MovieCard.jsx
@@ -9,7 +9,8 @@ const MovieCard = () => {
   useEffect(() => {
     const seeMovies = () => {
       return makeAPICall({
-        path: "https://api.themoviedb.org/3/movie/popular?api_key=3389f1e448c6a36c49905fe30d46d6fe&language=en-US&page=1",
+        path: "https://api.themoviedb.org/3/discover/movie?api_key=3389f1e448c6a36c49905fe30d46d6fe&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate",
+        // https://api.themoviedb.org/3/discover/movie?api_key=3d820eab8fd533d2fd7e1514e86292ea&language=en-US
         payload: null,
         method: "GET",
       })
@@ -25,26 +26,26 @@ const MovieCard = () => {
 
   return (
     <>
-      {state &&
-        state?.result?.map((item) => (
-          <>
-            <div className="movie--img">
-              <img
-                src={
-                  "https://api.themoviedb.org/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg"
-                }
-                alt=""
-              />
+      {state.length === 0 ? (
+        "No movie"
+      ) : (
+        <>
+          {state?.results?.map((item) => (
+            <div className="movie__cards">
+              <div className="movie--img">
+                <img src={`https://api.themoviedb.org${item.poster_path}`} alt="" />
+              </div>
+              <div className="movie--info">
+                <h3>{item.title}</h3>
+                <p>
+                  <span>{item.release_date}</span> |{" "}
+                  <span>{item.vote_average}</span>
+                </p>
+              </div>
             </div>
-            <div className="movie--info">
-              <h3>{item.title}</h3>
-              <p>
-                <span>{item.release_date}</span> |{" "}
-                <span>{item.vote_average}</span>
-              </p>
-            </div>
-          </>
-        ))}
+          ))}
+        </>
+      )}
     </>
   );
 };
